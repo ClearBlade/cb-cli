c384be6eba40d1aa6839322acbe713a4dcab8918
Clark Bynum <cbynum@clearblade.com>
2015-02-23 17:09:12 -0600

    now importing permissions for msgHistory, users, and push

--- import.go
+++ import.go
@@ -44,18 +44,26 @@ func CreateServices(cli *cb.DevClient, sysKey string, services []cb.Service) err
 	return nil
 }
 
+func CreateGenericRole(cli *cb.DevClient, sysKey, role, permission string, level int) error {
+	err := cli.AddGenericRole(sysKey, role, permission, level)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func CreateRoles(cli *cb.DevClient, sysKey string, roles []interface{}) error {
 	for i := 0; i < len(roles); i++ {
 		for k, v := range roles[i].(map[string]interface{})["Permissions"].(map[string]interface{}) {
-			if k == "CodeServices" {
+			switch k {
+			case "CodeServices":
 				for j := 0; j < len(v.([]interface{})); j++ {
 					err := cli.AddServiceToRole(sysKey, v.([]interface{})[j].(map[string]interface{})["Name"].(string), roles[i].(map[string]interface{})["ID"].(string), int(v.([]interface{})[j].(map[string]interface{})["Level"].(float64)))
 					if err != nil {
 						return err
 					}
 				}
-			}
-			if k == "Collections" {
+			case "Collections":
 				for j := 0; j < len(v.([]interface{})); j++ {
 					if roles[i].(map[string]interface{})["ID"].(string) != "Administrator" {
 						err := cli.AddCollectionToRole(sysKey, v.([]interface{})[j].(map[string]interface{})["ID"].(string), roles[i].(map[string]interface{})["ID"].(string), int(v.([]interface{})[j].(map[string]interface{})["Level"].(float64)))
@@ -63,6 +71,21 @@ func CreateRoles(cli *cb.DevClient, sysKey string, roles []interface{}) error {
 							return err
 						}
 					}
+				}
+			case "MsgHistory":
+				err := CreateGenericRole(cli, sysKey, roles[i].(map[string]interface{})["ID"].(string), "msgHistory", int(v.(map[string]interface{})["Level"].(float64)))
+				if err != nil {
+					return err
+				}
+			case "Push":
+				err := CreateGenericRole(cli, sysKey, roles[i].(map[string]interface{})["ID"].(string), "push", int(v.(map[string]interface{})["Level"].(float64)))
+				if err != nil {
+					return err
+				}
+			case "UsersList":
+				err := CreateGenericRole(cli, sysKey, roles[i].(map[string]interface{})["ID"].(string), "users", int(v.(map[string]interface{})["Level"].(float64)))
+				if err != nil {
+					return err
 				}
 			}
 		}
