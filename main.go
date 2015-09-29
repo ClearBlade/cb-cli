74603f3590c4df1b059494494e75876eda39bdbd
Steve Manweiler <swm@clearblade.com>
2015-09-28 20:40:43 -0500

    new flags processing

--- main.go
+++ main.go
@@ -5,14 +5,15 @@ import (
 	"fmt"
 	cb "github.com/clearblade/Go-SDK"
 	"github.com/clearblade/cblib"
+	"os"
 	"strings"
 )
 
 func init() {
-	flag.StringVar(&cblib.SchemaDir, "schemadir", ".", "Name of root directory of your export/import schema")
-	flag.StringVar(&cblib.URL, "url", "", "Set the URL of the platform you want to use")
-	flag.BoolVar(&cblib.ShouldImportCollectionRows, "importrows", false, "If supplied the import command will transfer collection rows from the old system to the new system")
-	flag.IntVar(&cblib.ImportPageSize, "pagesize", 100, "If supplied the import command will migrate the specified number of rows per request")
+	flag.StringVar(&cblib.URL, "url", "", "Platform URL")
+	flag.BoolVar(&cblib.Help, "help", false, "Print help message")
+	flag.StringVar(&cblib.Email, "email", "", "Developer username")
+	flag.StringVar(&cblib.Password, "password", "", "Developer password")
 }
 
 func main() {
@@ -22,74 +23,28 @@ func main() {
 	} else {
 		cblib.URL = cb.CB_ADDR
 	}
+	client, err := doAuth()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 	cmd := strings.ToLower(flag.Arg(0))
-	var err error
-	var sysKey, dir string
+	subCommand, err := cblib.GetCommand(cmd)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	subArgs := flag.Args()
+	err = subCommand.Execute(client, subArgs[1:])
+	if err != nil {
+		fmt.Printf("MAJOR FAIL: %s\n", err.Error())
+	}
+}
 
-	switch cmd {
-	case "init":
-		if err := cblib.Init_cmd(); err != nil {
-			fmt.Printf("Error initializing: %v\n", err)
-		}
-	case "pull":
-		if flag.NArg() != 2 {
-			fmt.Printf("pull requires the systemKey as an argument\n")
-		}
-		if err := cblib.Pull_cmd(flag.Arg(1)); err != nil {
-			fmt.Printf("Error pulling data: %v\n", err)
-		}
-	case "push":
-		if flag.NArg() != 2 {
-			sysKey, err = cblib.Sys_for_dir()
-			if err != nil {
-				fmt.Printf("%v\n", err)
-				return
-			}
-			dir = "."
-		} else {
-			dir = ""
-			sysKey = flag.Arg(1)
-		}
-		if err := cblib.Push_cmd(sysKey, dir); err != nil {
-			fmt.Printf("Error pushing: %v\n", err)
-		}
-	case "export":
-		if flag.NArg() != 2 {
-			fmt.Printf("export requires the systemKey as an argument\n")
-		}
-		if err := cblib.Export_cmd(flag.Arg(1), flag.Arg(2)); err != nil {
-			fmt.Printf("Error export data: %v\n", err)
-		}
-	case "import":
-		/*
-			var sysKey, dir string
-			var err error
-			sysKey, err = cblib.Sys_for_dir()
-			if err != nil {
-				fmt.Printf("%v\n", err)
-				fmt.Printf("%v\n", sysKey)
-				return
-			}
-			dir = "."
-
-			if err := cblib.Import_cmd(dir, flag.Arg(2)); err != nil {
-				fmt.Printf("Error import data: %v\n", err)
-			}
-		*/
-		if err := cblib.Import_cmd(); err != nil {
-			fmt.Printf("Error import data: %s\n", err.Error())
-		}
-	case "promote":
-		fmt.Printf("promote command not yet implemented\n")
-	case "diff":
-		fmt.Printf("diff command not yet implemented\n")
-	case "status":
-		fmt.Printf("status command not yet implemented\n")
-	case "test":
-		fmt.Printf("test command not yet implemented\n")
-	case "generate":
-		fmt.Printf("generate command not yet implemented\n")
-	default:
-		fmt.Printf("Commands: 'init', 'pull', 'push', 'export', 'import', 'promote', 'diff', 'status', 'test', 'generate'\n")
+func doAuth() (*cb.DevClient, error) {
+	if cblib.Email != "" && cblib.Password != "" {
+		return cblib.AuthUserPass(cblib.Email, cblib.Password)
+	} else {
+		return cblib.Auth("")
 	}
 }
