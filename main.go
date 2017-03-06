2f82961e0a0721047feefd634c548305a1ba21c5
Jim Bouquet <jbouquet@clearblade.com>
2017-03-06 16:41:07 -0600

    Added code to handle erroneous whitespace in arguments

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/clearblade/cblib"
 	"os"
+
+	"strings"
+
+	"github.com/clearblade/cblib"
 )
 
 func main() {
@@ -13,11 +16,27 @@ func main() {
 		os.Exit(1)
 	}
 	subCommand, err := cblib.GetCommand(theArgs[1])
+
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
-	err = subCommand.Execute( /*client,*/ theArgs[2:])
+
+	fmt.Printf("%v\n", theArgs[2:])
+
+	var newArgs []string
+
+	for _, value := range theArgs[2:] {
+		//if the first character of the value is a dash, we are at a new flag
+		if len(value) > 0 && string(value[0]) == "-" {
+			newArgs = append(newArgs, strings.TrimSpace(value))
+		} else {
+			//append the value to the previous element
+			newArgs[len(newArgs)-1] += strings.TrimSpace(value)
+		}
+	}
+
+	err = subCommand.Execute( /*client,*/ newArgs)
 	if err != nil {
 		fmt.Printf("Aborting: %s\n", err.Error())
 	}
