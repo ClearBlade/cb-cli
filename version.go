34717ded9cbb2d4606bcac753ff721a2b84e6a97
rreinold <rreinold@gmail.com>
2018-12-03 13:58:55 -0600

    Set version to 4.4.0-rc2

--- version.go
+++ version.go
@@ -1,8 +1,3 @@
-//
-//  Important -- when you're creating a new cb-cli version for github,
-//  set the cbCliVersion string var below before pushing to github.
-//  -swm
-//
 package main
 
-var cbCliVersion = "4.1.2"
+var cbCliVersion = "4.4.0-rc2"
